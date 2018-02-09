cd64046c94ee6fabfec2af3639f75508ff87e440
Artem Bondarenko <abondarenko@odin.com>
2018-02-09 20:02:07 +0300

    Add sample to work with items list

--- app/src/main/java/com/my/backery/MainActivity.java
+++ app/src/main/java/com/my/backery/MainActivity.java
@@ -1,11 +1,18 @@
 package com.my.backery;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +24,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_menu:
+//                    showMenu();
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_orders:
+//                    mTextMessage.setText(R.string.title_orders);
+                    mTextMessage.setText("amy text");
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -40,6 +48,60 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        ListView lv = (ListView) findViewById(R.id.list_view);
+        lv.setAdapter(new MyListAdapter(this, R.layout.menu_item, Arrays.asList("a", "b", "c")));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "from OnCreate " + position, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
+    private class MyListAdapter extends ArrayAdapter<String> {
+        private int layout;
+
+        private MyListAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.thumb = (ImageView) convertView.findViewById(R.id.menu_item_thumb);
+                if (viewHolder.thumb != null)
+                    viewHolder.thumb.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(getContext(), "Item clicked " + position, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                viewHolder.title = (TextView) convertView.findViewById(R.id.menu_item_text);
+                viewHolder.button = (Button) convertView.findViewById(R.id.menu_item_btn);
+                convertView.setTag(viewHolder);
+            }
+            ViewHolder mainViewHolder = (ViewHolder) convertView.getTag();
+            mainViewHolder.title.setText(getItem(position));
+            mainViewHolder.button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Button clicked " + position, Toast.LENGTH_LONG).show();
+                }
+            });
+
+            return convertView;
+        }
+    }
+
+    public class ViewHolder {
+        ImageView thumb;
+        TextView title;
+        Button button;
+    }
 }
